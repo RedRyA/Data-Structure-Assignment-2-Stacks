6114dd5b3ed5bd2baf58b6efb51085e7e9be29e5
RedRyA <reddoch.ryan@yahoo.com>
2023-02-09 23:11:42 -0600

    most postfix work now

--- booleanEvaluation.c
+++ booleanEvaluation.c
@@ -1,13 +1,11 @@
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
 
-#include "stack.h"
 #include "booleanEvaluation.h"
 #include "booleanWithError.h"
-
-
+#include "stack.h"
 
 /* TODO
  * printName
@@ -16,137 +14,224 @@
  *
  * Prints name of the student who worked on this solution
  */
-void printName( )
+void printName()
 {
     /* TODO : Fill in your name */
     printf("This solution was completed by:\n");
     printf("<Ryan Reddoch>\n");
 }
 
-
-
-
 /* TODO
  * evaluatePostfix
- * input: a postfix expression (calls tokenizeString to get the tokens from the string)
- * output: T, F, or E
+ * input: a postfix expression (calls tokenizeString to get the tokens from the
+ * string) output: T, F, or E
  *
- * Uses a stack to evaluates the postfix expression and returns the result as a string where "T" denotes true and "F" denotes false.
- * If the postfix expression is invalid it returns "E" to denote an error has occurred.  Be sure to use the helpful functions in booleanEvaluation.
+ * Uses a stack to evaluates the postfix expression and returns the result as a
+ * string where "T" denotes true and "F" denotes false. If the postfix
+ * expression is invalid it returns "E" to denote an error has occurred.  Be
+ * sure to use the helpful functions in booleanEvaluation.
  *
  * Hints:
  * - DO NOT free the parameter str.  It is freed by the calling function.
  * - Be sure to use the helpful functions in booleanEvaluation.
- * - Test your code often!  Get it working correctly for the first couple test cases instead of trying to solve all of them at once.
+ * - Test your code often!  Get it working correctly for the first couple test
+ * cases instead of trying to solve all of them at once.
  */
-char *evaluatePostfix( char *str )
+char *evaluatePostfix(char *str)
 {
 
-    Stack *s= createStack();
-  
+    Stack *s = createStack();
+
+    char *op1;   // holds 1st operand
+    char *op2;   // holds 2nd operand
+    char *x[10]; // holds operator;
+    char *testArray[] = {"NOT", "AND", "NAND", "OR",
+                         "NOR", "XOR", "COND", "BICOND"};
+    int i = 0;
+    int count = 0;
+    char bool2str;
+    int boolyTest;
+    int strTest;
+    /*TOKEN  */
+    int tokenCount;
+    boolean  res;
+    char **arrTokens = tokenizeString(str, &tokenCount);
+
+    op1 = (char *)malloc(sizeof(char)*count);
+    op2 = (char *)malloc(sizeof(char)*count);
+    /* Your variable Declarations: */
+
+    /* Currently commented out to prevent a memory leak.  Uncomment to get the
+     * tokens from str. */
+
+    /* ARRTOKENS HOLD T F IN THE i AND OPERATORS IN J, */
 
 
-int op1; // holds 1st operand
-int op2; // holds 2nd operand
-char* x[10];  // holds operator;
-char* testArray[]= {"NOT","AND","NAND","OR","NOR","XOR","COND","BICOND"};
-int i=0;
-int j=0;
-int count=0;
-boolean bool;
-char bool2str;
- int boolyTest;
- int strTest;
-     /*TOKEN  */
-     int tokenCount;
- char **arrTokens = tokenizeString(str, &tokenCount);
+    for (i = 0; i < tokenCount; i++)
+    {
 
- op1 = (int *)malloc(sizeof(int) * count);
- op2 = (int *)malloc(sizeof(int) * count);
- /* Your variable Declarations: */
+        if (strequals(arrTokens[i], "T") || strequals(arrTokens[i], "F"))
+        {
 
- /* Currently commented out to prevent a memory leak.  Uncomment to get the tokens from str. */
+            count++;
+            push(s, arrTokens[i]);
+            
+                
 
- /* ARRTOKENS HOLD T F IN THE i AND OPERATORS IN J, */
- /* iterate through arrTokens, push operands in stack */
-  for (i=0;i<tokenCount;i++) {
-  for (j=0;j<tokenCount;j++){
 
-      if (strcmp(arrTokens[i][j], 'T') || strcmp(arrTokens[i][j], 'F'))   
-      {
-        // this takes 
-          bool = stringToBoolean(&arrTokens[i][j]);
+            
+        }
+                       /* NOT START */
 
-          push(s, &bool);
-          printf("%s TEST ME BRAH \n", top(s));
-          j++;
+        if (strequals(arrTokens[i], "NOT"))
+        {
+             op1 = pop(s);
+            if(strequals(op1,"T")){
 
-// THIS X HOLDS WHAT I NEED 
-x[i]=arrTokens[j];
+                op1="F";
+                res = stringToBoolean(op1);
+                
+             }
 
- printf(" X Value %s \n ",x[0]);
+             else if (strequals(op1, "F"))
+             {
+                op1="T";
+               res = stringToBoolean(op1);
+             }
+        }
+                      /*NOT END */
 
-      }
- 
-  
-  }
-   i++;    
-  }
+                    /*AND START*/
+        if (strequals(arrTokens[i], "AND") && count >=1){
+             op2 = pop(s);
+             op1 = pop(s);
 
-  /*TRIED TO SET X[0] TO A VAR BUT DID NOT WORK */
-/////////////////////////////////
-  /* NOT TEST */
-   strTest = strcmp(x[0], "NOT");
+             if (strequals(op1, "T")&&strequals(op2, "T"))
+             {
+               res = stringToBoolean(op1);
+            }
+            else{
+                res=stringToBoolean("F");
 
-  if (strTest = 0)
-  {
+            }
+                }
+                    /*AND END*/
 
-int op1=pop(s);
- bool2str=booleanToString(boolyTest);
- boolyTest=!op1;
+                    /*NAND START*/
 
- printf("%d !Not test \n",boolyTest);
-push(s,boolyTest);
-free(op1);
-free(x);
- return boolyTest;
-  }
+                if (strequals(arrTokens[i], "NAND") && count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
 
-  ////////// AND TEST //////////////
+            if (strequals(op1, "T") && strequals(op2, "T"))
+            {
+                res = stringToBoolean("F");
+            }
+            else
+            {
+                res = stringToBoolean("T");
+            }
+                }
+                    /*END NAND*/
 
-   strTest = strcmp(x[0], "AND");
+                    /*START OR*/
+                if (strequals(arrTokens[i], "OR") && count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
 
-  if (strTest = 0)
-  {
- printf("%d AND test\n ");
- int op2 = pop(s);
- op1=pop(s);
-  bool2str = booleanToString(boolyTest);
-  boolyTest = (op1 && op2);
- push(s, boolyTest);
- printf("%s Top TEST \n", top(s));
- free(op1);
- free(op2);
- free(x);
- freeStackElements(s);
- freeStack(s);
- return boolyTest;
-  }
-  /////////////////////////////////////
+            if (strequals(op1, "T") || strequals(op2, "T"))
+            {
+                res = stringToBoolean("T");
+            }
+            else
+            {
+                res = stringToBoolean("F");
+            }
+                }
+                    /*END OR*/
+                    
+                    /*START XOR*/
+                if (strequals(arrTokens[i], "XOR") && count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
 
-  // freeStackElements(&s);
-  // freeStack(&s);
-  return booleanToString(boolyTest);
-  /* Replace this with your actual solution to return */
+            if (strequals(op1, "T") && strequals(op2, "F") || strequals(op1, "F") && strequals(op2, "T"))
+            {
+                res = stringToBoolean("T");
+            }
+            else
+            {
+                res = stringToBoolean("F");
+            }
+                }
+                    /*END XOR*/
 
+                    /*START NOR*/
+                if (strequals(arrTokens[i], "NOR") && count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
+
+            if (strequals(op1, "F") && strequals(op2, "F"))
+            {
+                res = stringToBoolean("T");
+            }
+            else
+            {
+                res = stringToBoolean("F");
+            }
+                }
+                    /*END NOR*/
+
+                    /*START COND*/
+                if (strequals(arrTokens[i], "COND") && count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
+
+            if (strequals(op1, "T") && strequals(op2, "F"))
+            {
+                res = stringToBoolean("F");
+            }
+            else
+            {
+                res = stringToBoolean("T");
+            }
+                }
+                    /*END COND*/
+
+                    /*BICOND START */
+                if (strequals(arrTokens[i], "BICOND") &&  count >= 1)
+                {
+            op2 = pop(s);
+            op1 = pop(s);
+
+            if (strequals(op1, "T") && strequals(op2, "T") || strequals(op1, "F") &&strequals(op2, "F" ))
+            {
+                res = stringToBoolean("T");
+            }
+            else
+            {
+                res = stringToBoolean("F");
+            }
+                }
+                    /*BICOND END*/
+
+printf("COUNT TOTAL = %d \n",count);
+    }         
+       return  booleanToString(res);
+    /* Replace this with your actual solution to return */
 }
-//}
 
+//}
 
 /* TODO
  * postfixToInfix
- * input: a postfix expression (calls tokenizeString to get the tokens from the string)
- * output: the equivalent infix expression
+ * input: a postfix expression (calls tokenizeString to get the tokens from the
+ * string) output: the equivalent infix expression
  *
  * Uses a stack to convert str to its equivalent expression in infix.
  * You can assume that the postfix expression is valid
@@ -154,37 +239,35 @@ free(x);
  * Hints:
  * - DO NOT free the parameter str.  It is freed by the calling function.
  * - Be sure to use the helpful functions in booleanEvaluation.
- * - Test your code often!  Get it working correctly for the first couple test cases instead of trying to solve all of them at once.
+ * - Test your code often!  Get it working correctly for the first couple test
+ * cases instead of trying to solve all of them at once.
  */
 
 //////////////////////////////////
 ///////////////////////////////
 
-
-
-char *postfixToInfix( char *str )
+char *postfixToInfix(char *str)
 {
- /*   
-Stack s;
-int count; //counts tokens
-char* op1; // holds 1st operand
-char* op2; // holds 2nd operand
-char* x;  // holds operator;
-Stack createStack();
-*/
+    printf("p2I \n");
+    /*
+    
+   Stack s;
+   int count; //counts tokens
+   char* op1; // holds 1st operand
+   char* op2; // holds 2nd operand
+   char* x;  // holds operator;
+   Stack createStack();
+   */
     /* Your variable Declarations: */
- //   int tokenCount;
- //   char** arrTokens = tokenizeString( str, &tokenCount );  /* Currently commented out to prevent a memory leak.  Uncomment to get the tokens from str. */
-
-
-
+    //   int tokenCount;
+    //   char** arrTokens = tokenizeString( str, &tokenCount );  /* Currently
+    //   commented out to prevent a memory leak.  Uncomment to get the tokens from
+    //   str. */
 
     /* Your code: */
 
-
-
-
-    return booleanToString( ERROR ); /* Replace this with your actual solution to return */
+   // return booleanToString(
+   //     ERROR); /* Replace this with your actual solution to return */
 }
 
 /* You may want to create some helper functions down here! */
